ed57f8ad798720bfbdd788059a7e16ab5423c3af
Pranav Sreedhar <pranav@Pranavs-Macbook-Pro.local>
2021-06-28 22:51:22 +0530

    channel thumbnail

--- src/types/YTChannel.ts
+++ src/types/YTChannel.ts
@@ -1,5 +1,4 @@
 export type YTChannel = {
-<<<<<<< HEAD
     items: YTChannelItem[];
   };
   export type YTChannelItem = {
@@ -40,44 +39,3 @@ export type YTChannel = {
     width: number;
     height: number;
   };
-=======
-  items: YTChannelItem[];
-};
-export type YTChannelItem = {
-  etag: string;
-  id: YTChannelId;
-  videoId?: string;
-  kind: string;
-  snippet: YTChannelSnippet;
-  subscribers?: number;
-  views?: number;
-};
-
-export type YTChannelId = {
-  kind: string;
-  videoId: string;
-};
-
-export type YTChannelSnippet = {
-  publishedAt: string;
-  channelId: string;
-  title: string;
-  description: string;
-  thumbnails: YTThumbNail;
-  channelTitle: string;
-  liveBroadcastContent: string;
-  publishTime: string;
-};
-
-export type YTThumbNail = {
-  default: YTThumbNailData;
-  medium: YTThumbNailData;
-  high: YTThumbNailData;
-};
-
-export type YTThumbNailData = {
-  url: string;
-  width: number;
-  height: number;
-};
->>>>>>> d185f9a808889693226c6869efa083fd1ce43dbc
--- src/utils/Helpers.ts
+++ src/utils/Helpers.ts
@@ -1,32 +1,4 @@
 export namespace Helpers {
-<<<<<<< HEAD
-    export const getUniqueArray = (
-      redundantArray: any,
-      distinctKey: string,
-    ): any => {
-      const result = [];
-      const distinctArray = new Map();
-      for (const item of redundantArray) {
-        if (!distinctArray.has(item[distinctKey])) {
-          distinctArray.set(item[distinctKey], true);
-          result.push(item);
-        }
-      }
-      return result;
-    };
-  
-    export const sortArrayByKey = (array: any, key: string) => {
-      return array.sort((a: any, b: any) => {
-        return a[key] - b[key];
-      });
-    };
-
-    export const reverseArray = (array: any) => {
-        return array.reverse();
-    };
-    
-  }
-=======
   export const getUniqueArray = (
     redundantArray: any,
     distinctKey: string,
@@ -41,11 +13,13 @@ export namespace Helpers {
     }
     return result;
   };
-
   export const sortArrayByKey = (array: any, key: string) => {
     return array.sort((a: any, b: any) => {
       return a[key] - b[key];
     });
   };
+
+  export const reverseArray = (array: any) => {
+    return array.reverse();
+  };
 }
->>>>>>> d185f9a808889693226c6869efa083fd1ce43dbc
