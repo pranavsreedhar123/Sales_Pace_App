fd9a8ee977a3b1f57a5492273f47c6c6ed753731
Nidhish <Nidhish.Nade@ext.saint-gobain.com>
2021-08-25 14:51:17 +0530

    git commit wednesday sso token implemented

--- src/services/govtProjectsAPI.ts
+++ src/services/govtProjectsAPI.ts
@@ -1,5 +1,71 @@
+import { Tender_OAuth_ACCESS_TOKEN } from '../utils/Constant';
 import {apiClient} from './apiClient';
 
+
+export async function getAccessTokenForAPI(): Promise<object>{
+  const accessToken=(await apiClient(
+`https://test.api.saint-gobain.com/sgdsi/oauth/token`,
+{
+  method:'POST',
+  body: JSON.stringify(
+    {    client_id:Tender_OAuth_ACCESS_TOKEN.client_id,
+         client_secret:Tender_OAuth_ACCESS_TOKEN.client_secret,
+         scope:Tender_OAuth_ACCESS_TOKEN.scope,
+         
+}),
+      headers: {
+        'Accept':       '*/*',
+        'Content-Type': 'application/x-www-form-urlencoded',
+       
+      }
+}
+  )) as object;
+  
+  return accessToken;
+}
+export async function getHousingDataAxwayAPI(
+  access_token:string
+): Promise<object>{
+  let formBody :any= [];
+ 
+  let encodedKey = encodeURIComponent('client_id');
+  let encodedValue = encodeURIComponent(SSO_OAuth_Get_ACCESS_TOKEN.clientId);
+  formBody.push(encodedKey + "=" + encodedValue);
+   encodedKey = encodeURIComponent('client_secret');
+   encodedValue = encodeURIComponent(SSO_OAuth_Get_ACCESS_TOKEN.clientSecret);
+  formBody.push(encodedKey + "=" + encodedValue);
+   encodedKey = encodeURIComponent('scope');
+   encodedValue = encodeURIComponent(SSO_OAuth_Get_ACCESS_TOKEN.scope);
+  formBody.push(encodedKey + "=" + encodedValue);
+   encodedKey = encodeURIComponent('grant_type');
+   encodedValue = encodeURIComponent(SSO_OAuth_Get_ACCESS_TOKEN.grant_type);
+  formBody.push(encodedKey + "=" + encodedValue);
+   encodedKey = encodeURIComponent('username');
+   encodedValue = encodeURIComponent(userName);
+  formBody.push(encodedKey + "=" + encodedValue);
+  encodedKey = encodeURIComponent('password');
+  encodedValue = encodeURIComponent(password);
+ formBody.push(encodedKey + "=" + encodedValue);
+  const axwayHousingData=(await apiClient(
+`https://test.api.saint-gobain.com/sgdsi/oauth/token`,
+{
+  method:'POST',
+  body: JSON.stringify(
+    {    client_id:Tender_OAuth_ACCESS_TOKEN.client_id,
+         client_secret:Tender_OAuth_ACCESS_TOKEN.client_secret,
+         scope:Tender_OAuth_ACCESS_TOKEN.scope,
+         
+}),
+      headers: {
+        'Accept':       '*/*',
+        'Content-Type': 'application/x-www-form-urlencoded',
+       
+      }
+}
+  )) as object;
+  
+  return accessToken;
+}
 export async function getHousingDataAPI(): Promise<string> {
   const apiHousingData = (await apiClient(
     `http://l01svindeca0101.zl.if.atcsg.net:6565/Api/Tender/GetTenderList?page_size=10&page_number=0`,
